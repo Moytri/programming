c6ce053d3bd62941af7bab61cf3effa2c585f63a
moytri <moytritopa@gmail.com>
2018-07-04 11:16:08 -0700

    bubble sort using array

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+class  BubbleSort {
+
+	public static void main(String [] args){
+		
+		int [] arr = {10, 4, 3, 5, 7, 9};
+                BubbleSort sortedArr = new BubbleSort();
+
+		arr = sortedArr.bubbleSort(arr);
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}	
+	}
+
+	private int[] bubbleSort(int [] arr) {
+
+		for(int i = 0; i<arr.length; i++){             //for each pass
+			for(int j = 0; j<arr.length-1; j++){   //for the comparism between adjacent elements in each pass
+				if(arr[j] > arr[j+1]) { 
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+}
