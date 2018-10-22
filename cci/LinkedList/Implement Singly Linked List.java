830f5a342805bf409d530c78fcbf1eb402c94af0
moytri <moytritopa@gmail.com>
2018-10-21 19:00:58 -0700

    list

---
+++ cci/LinkedList/Implement Singly Linked List.java
@@ -0,0 +1,43 @@
+package cciLinkedList;
+
+public class SinglyLinkedList {
+	private static int size = 0;
+	protected static Node head = null;
+	public static void main(String[] args) {
+		add(2);
+		add(4);
+		add(1);
+		print(head);
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public static void add(int data) {
+		Node newNode = new Node(data);
+		if(head == null) head = newNode;
+		else {
+			newNode.next = head;
+			head = newNode;
+		}
+		size++;
+	}
+	
+	public static void print(Node head) {
+		while(head != null) {
+			System.out.print(head.val + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
+	
+	static class Node {
+		int val;
+		Node next;
+		public Node(int val) {
+			this.val = val;
+			this.next = null;
+		}
+	}
+}
