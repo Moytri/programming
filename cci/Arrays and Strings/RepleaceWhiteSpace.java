26be35aec716a49ef13adcb43ae393798323c9f4
moytri <moytritopa@gmail.com>
2019-01-03 16:55:07 -0800

    Update RepleaceWhiteSpace.java

--- cci/Arrays and Strings/RepleaceWhiteSpace.java
+++ cci/Arrays and Strings/RepleaceWhiteSpace.java
@@ -19,8 +19,8 @@ public class RepleaceWhiteSpace {
 	public static String URLify(String str) {
 		StringBuilder sb = new StringBuilder();		
 		for(int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            sb.append(ch == ' ' ? "%20" : ch);
+                   char ch = str.charAt(i);
+                   sb.append(ch == ' ' ? "%20" : ch);
 		}
 		return sb.toString();
 	}
