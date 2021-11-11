e5478eb55e6b212dae5749ba3d7f4d8c225f112b
Moytri <moytritopa@gmail.com>
2021-11-10 17:29:22 -0800

    Number of Arrays

---
+++ leetCode/slidingWindow/src/slidingWindow/NumberOfSubArrays.java
@@ -0,0 +1,26 @@
+package slidingWindow;
+
+public class NumberOfSubArrays {
+
+	public static void main(String[] args) {
+		NumberOfSubArrays numberOfSubArrays = new NumberOfSubArrays();
+		System.out.println(numberOfSubArrays.numberOfSubarrays(new int[] {1,1,2,1,2,1}, 3));
+	}
+	
+	public int numberOfSubarrays(int[] nums, int k) {
+        int res = 0, i = 0, count = 0, n = nums.length;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] % 2 == 1) {
+                --k;
+                count = 0;
+            }
+            while (k == 0) {
+                if (nums[i++] % 2 == 1) ++k;
+                ++count;
+            }
+            res += count;
+        }
+        return res;
+    }
+
+}
