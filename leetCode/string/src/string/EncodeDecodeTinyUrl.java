bfa217be11154f28f8588c771632692661315169
Moytri <moytritopa@gmail.com>
2021-11-11 08:27:15 -0800

    Encode Decode TinyUrl

---
+++ leetCode/string/src/string/EncodeDecodeTinyUrl.java
@@ -0,0 +1,32 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 
+ * @author Moytri
+ * Problem: https://leetcode.com/problems/encode-and-decode-tinyurl/
+ *
+ */
+public class EncodeDecodeTinyUrl {
+
+	public static void main(String[] args) {
+		EncodeDecodeTinyUrl url = new EncodeDecodeTinyUrl();
+		String tiny = url.encode("https://leetcode.com/problems/design-tinyurl");
+		System.out.println(url.decode(tiny));
+	}
+    Map<Integer, String> map = new HashMap();
+    String host = "http://tinyurl.com/";
+
+    public String encode(String longUrl) {
+      int key = longUrl.hashCode();
+      map.put(key, longUrl);
+      return host+key;
+    }
+
+    public String decode(String shortUrl) {
+      int key = Integer.parseInt(shortUrl.replace(host,""));
+      return map.get(key);
+    }
+
+}
