3040d81ab78b59fe3f3c3239376bbbafc269fdb3
Moytri <moytritopa@gmail.com>
2021-12-23 16:36:56 -0800

    graph dfs

---
+++ leetCode/graph-ds/src/dfs/PathExistsInGraph.java
@@ -0,0 +1,53 @@
+package dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PathExistsInGraph {
+
+	public static void main(String[] args) {
+		PathExistsInGraph pathExists = new PathExistsInGraph();
+		System.out.println(pathExists.validPath(
+				3,
+				new int [][] {
+					{0, 1},
+					{1, 2},
+					{2, 0}
+				},
+				0,
+				2));
+	}
+	
+	public boolean validPath(int n, int[][] edges, int start, int end) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        boolean [] visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+        
+        for (int[] edge : edges) {
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(start);
+        
+        while(!stack.isEmpty()) {
+            int current = stack.pop();
+            
+            if (current == end) return true;
+            
+            List<Integer> ls = adjacencyList.get(current);
+            
+            for (int i : ls) {
+                if (!visited[i]) stack.add(i);
+            }
+            visited[current] = true;
+        }
+        
+        return false;
+    }	
+}
---
+++ leetCode/graph-ds/src/dfs/TraverseAllPathBtnTwoVertices.java
@@ -0,0 +1,75 @@
+package dfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 
+ * @author https://leetcode.com/problems/all-paths-from-source-to-target/discuss/118713/Java-DFS-Solution
+ * Problem: https://leetcode.com/problems/all-paths-from-source-to-target/
+ *
+ */
+public class TraverseAllPathBtnTwoVertices {
+
+	public static void main(String[] args) {
+		TraverseAllPathBtnTwoVertices allPath = new TraverseAllPathBtnTwoVertices();
+		allPath.allPathsSourceTarget(new int[][] {
+			{1,2},
+			{3},
+			{3},
+			{}
+		});
+	}
+	
+	 public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+	    Integer end = graph.length - 1;
+	    List<List<Integer>> result = new ArrayList<>();
+	
+	    Deque<Integer> stack = new ArrayDeque<>(); // keep track of nodes to process
+	    Deque<Integer> path  = new ArrayDeque<>(); // path generated so far
+	    
+	    // prime the stack with the start node
+	    stack.push(0);
+	    
+	    while (!stack.isEmpty()) {
+	        /* 'hack' alert
+	        we need an Integer object and not an int because path.peekLast()
+	        will return null the very first time it is called.
+	        you cannot compare null with an int but you can with an Integer
+			*/
+	        Integer current = stack.peek(); /* peek, don't pop */
+	        if (current == path.peekLast()) {
+	            /*
+				   when the top of the stack and the top of queue match
+	               it means we have processes all this node's connections
+	               there are no more paths to explore so remove the node
+	               from the list of nodes to process, and from the path
+	               We are backtracking to the previous point in the path, and
+	               the list of nodes to process
+				*/
+	            stack.pop();       // remove the end from the path
+	            path.removeLast(); // remove the end from the path
+	            continue;
+	        }
+	        path.offer(current);
+	        if (current == end) {
+	            /* We've found a path */
+	            result.add(new ArrayList(path)); /* add the path Queue as a List to the result */
+	            path.removeLast(); /* go back one step in the path */
+	            stack.pop();       /* remove the from the nodes to process */
+	            continue;
+	        }
+			/*
+			   we haven't seen this node before.
+	           add all of this node's neighbors to the stack of nodes to process
+			*/
+	        for (int neighbor : graph[current]) {
+	            stack.push(neighbor);
+	        }
+	    }
+	    return result;
+	}	
+
+}
